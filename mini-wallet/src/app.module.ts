b066a16a1260e960e3556a8adb0f54ab22f9b2fc
Fendy Heryanto <fendy3002@gmail.com>
2022-07-12 21:07:09 +0800

    remove unused import

--- mini-wallet/src/app.module.ts
+++ mini-wallet/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { ApiV1Module } from './api/v1/api-v1.module';
 import { GlobalModule } from './global.module';
-import { ServicesModule } from './services/services.module';
 
 @Module({
   imports: [GlobalModule, ApiV1Module],
